d945924e368ebdf50473d0e27db7a3f49ac69d4f
kfleung129 <endyjjhk@gmail.com>
2023-11-17 19:02:00 +0800

    modified canvas

--- src/index.js
+++ src/index.js
@@ -1,6 +1,33 @@
 let handpose, video, detections;
+
+const blockWidth = 625, blockHeight = 625, blockMargin = 30;
+let img, event;
+let modes = {
+    "f": function freeHandDrawing() {
+      console.log("freeHandDrawing");
+      drawKeypointsTmp();
+    },
+    "s": function regionSelection() {
+      console.log("regionSelection");
+      drawKeypointsTmp();
+    },
+    "c": function regionCopy() {
+      console.log("regionCopy");
+      drawKeypointsTmp();
+    },
+    "v": function regionPaste() {
+      console.log("regionPaste");
+      drawKeypointsTmp();
+    }
+};
+
+function preload() {
+  img = loadImage("./asset/test.jpg");
+}
+
 function setup() {
-  createCanvas(625, 437);
+  // createCanvas(625, 437);
+  createCanvas(1600, 900);
   video = createCapture(VIDEO);
   video.size(width, height);
   video.hide();
@@ -13,7 +40,9 @@ function gotResults(results) {
   detections = results;
 }
 function draw() {
-  image(video, 0, 0, width, height);
+  background(120);
+  image(img, 0, 0, blockWidth, blockHeight);
+  image(img, blockWidth + blockMargin, 0, blockWidth, blockHeight);
   if (detections) {
     if (detections.length > 0) drawKeypoints();
   }
@@ -29,3 +58,15 @@ function drawKeypoints() {
     }
   }
 }
+function drawKeypointsTmp() {
+  noStroke();
+  fill(255, 0, 0);
+  ellipse(500, 500, 10, 10);
+}
+window.addEventListener("keyup", (e) => {
+    let key = e.key;
+    mode = modes[e.key];
+    if(mode) mode();
+    document.getElementById("mode").innerHTML = mode ? mode.name : null;
+    e.preventDefault();
+})
